34a888a155cf37b7e78b8886500985aee1795e7c
Peter Kim <peyterkim@alum.lehigh.edu>
2023-02-21 11:07:26 -0500

    Initial commit

---
+++ src/store/slices/moviesSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { reset } from "../actions";
+
+const moviesSlice = createSlice({
+  name: "movie",
+  initialState: [],
+  reducers: {
+    addMovie(state, action) {
+      state.push(action.payload);
+    },
+    removeMovie(state, action) {
+      state.splice(state.indexOf(action.payload), 1);
+    }
+  },
+  extraReducers: (builder) => {
+    builder.addCase(reset, (state, action) => {
+      return [];
+    });
+  }
+});
+
+export const { addMovie, removeMovie } = moviesSlice.actions;
+export const moviesReducer = moviesSlice.reducer;
